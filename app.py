cee2d0aaeb4752fb1d623f08cf3a81b85ca3147d
MLProjects <titustraining4@gmail.com>
2020-10-01 09:23:27 +0530

    Removed sql lite

--- app.py
+++ app.py
@@ -1,25 +1,25 @@
 from flask import Flask, render_template, request
-# from chatterbot import ChatBot
-# from chatterbot.trainers import ChatterBotCorpusTrainer
+from chatterbot import ChatBot
+from chatterbot.trainers import ChatterBotCorpusTrainer
 import pickle
 
 app = Flask(__name__)
 
 model = pickle.load(open("nltk.pkl", 'rb'))
 
-# english_bot = ChatBot("Chatterbot", storage_adapter='chatterbot.storage.SQLStorageAdapter',database_uri='sqlite:///db.sqlite3')
-# trainer = ChatterBotCorpusTrainer(english_bot)
-# trainer.train("./greetings.yml")
+english_bot = ChatBot("Chatterbot")
+trainer = ChatterBotCorpusTrainer(english_bot)
+trainer.train("./greetings.yml")
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
-# @app.route("/chatterbot")
-# def get_bot_response():
-#     print(request)
-#     userText = request.args.get('msg')
-#     return str(english_bot.get_response(userText))
+@app.route("/chatterbot")
+def get_bot_response():
+    print(request)
+    userText = request.args.get('msg')
+    return str(english_bot.get_response(userText))
 
 @app.route("/chat-nltk")
 def get_response():
